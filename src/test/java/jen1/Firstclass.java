1a5e0a29eb0ddac057ee6c23e338e94686c8e7a4
yudhv <jpkaul2008@gmail.com>
2024-02-16 13:34:40 +0530

    JP

--- src/test/java/jen1/Firstclass.java
+++ src/test/java/jen1/Firstclass.java
@@ -4,10 +4,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Firstclass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
 		
 		ChromeDriver driver = new ChromeDriver();
+		Thread.sleep(5000);
+		driver.manage().window().maximize();
 		driver.get("https://www.google.com");
 		
 
