f975b56536952cc9d3f301b1650b881d6789a3bf
yudhv <jpkaul2008@gmail.com>
2024-02-16 13:16:09 +0530

    JP

---
+++ src/test/java/jen1/Firstclass.java
@@ -0,0 +1,16 @@
+package jen1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+
+	}
+
+}
